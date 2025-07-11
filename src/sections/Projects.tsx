2e090e746c087bb7a7f30a8df5fb4ef29b26de33
FushionHub Technology <180083626+FushionHub@users.noreply.github.com>
2025-07-11 08:37:31 +0100

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -10,40 +10,41 @@ import {Card} from "@/components/Card";
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "FushionHub Ai",
+    year: "2024",
+    title: "Fus Ai Bot",
     results: [
+      { title: "Was Version 1.0 of the bot" },
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://github.com/FushionHub/fus-aibots",
+    image: aiStartupLandingPage,
   },
   {
     company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    year: "2023",
+    title: "Cloud Infrastructure App",
     results: [
       { title: "Boosted sales by 20%" },
       { title: "Expanded customer reach by 35%" },
       { title: "Increased brand awareness by 15%" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: "https://me.fushionhubai.com.ng/projects/cloud-infrastructure-setup",
     image: lightSaasLandingPage,
   },
   {
     company: "Quantum Dynamics",
     year: "2023",
-    title: "AI Startup Landing Page",
+    title: "CRM Website",
     results: [
       { title: "Enhanced user experience by 40%" },
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    link: "https://me.fushionhubai.com.ng/projects/crm-system",
+    image: darkSaasLandingPage,
   },
 ];
 
